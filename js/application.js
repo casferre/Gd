f4d533234bcbe9be973704bf93e1ee2c4c27f831
Cassio Ferreira <cassioferre@gmail.com>
2017-11-01 23:02:09 -0200

    Marco_zero

---
+++ js/application.js
@@ -0,0 +1,147 @@
+$(document).ready(function() {
+    $('.carousel.carousel-slider').carousel();
+    });
+    $(document).ready(function(){
+        // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+        $('#modales').modal({ 
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .6, // Opacity of modal background
+            inDuration: 500, // Transition in duration
+            outDuration: 500, // Transition out duration
+            startingTop: '5%', // Starting top style attribute
+            endingTop: '15%', // Ending top style attribute
+            }
+            );
+        });
+    $(document).ready(function(){
+        // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+        $('#modalga').modal({ 
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .6, // Opacity of modal background
+            inDuration: 500, // Transition in duration
+            outDuration: 500, // Transition out duration
+            startingTop: '5%', // Starting top style attribute
+            endingTop: '15%', // Ending top style attribute
+            }
+            );
+        });
+    $(document).ready(function(){
+        // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+        $('#modalem').modal({ 
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .6, // Opacity of modal background
+            inDuration: 500, // Transition in duration
+            outDuration: 500, // Transition out duration
+            startingTop: '5%', // Starting top style attribute
+            endingTop: '15%', // Ending top style attribute
+            }
+            );
+        });
+    $(document).ready(function(){
+        // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+        $('#modalet').modal({ 
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .6, // Opacity of modal background
+            inDuration: 500, // Transition in duration
+            outDuration: 500, // Transition out duration
+            startingTop: '5%', // Starting top style attribute
+            endingTop: '15%', // Ending top style attribute
+            }
+            );
+        });
+    $(document).ready(function(){
+        // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+        $('#modalfh').modal({ 
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .6, // Opacity of modal background
+            inDuration: 500, // Transition in duration
+            outDuration: 500, // Transition out duration
+            startingTop: '5%', // Starting top style attribute
+            endingTop: '15%', // Ending top style attribute
+            }
+            );
+        });
+        $(document).ready(function(){
+            // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+        $('#modalga').modal({ 
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .6, // Opacity of modal background
+            inDuration: 500, // Transition in duration
+            outDuration: 500, // Transition out duration
+            startingTop: '5%', // Starting top style attribute
+            endingTop: '15%', // Ending top style attribute
+            }
+            );
+        });
+   $(document).ready(function(){
+        // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+        $('#modaljm').modal({ 
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .6, // Opacity of modal background
+            inDuration: 500, // Transition in duration
+            outDuration: 500, // Transition out duration
+            startingTop: '5%', // Starting top style attribute
+            endingTop: '15%', // Ending top style attribute
+            }
+            );
+        });
+    $(document).ready(function(){
+        // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+        $('#modalkg').modal({ 
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .6, // Opacity of modal background
+            inDuration: 500, // Transition in duration
+            outDuration: 500, // Transition out duration
+            startingTop: '5%', // Starting top style attribute
+            endingTop: '15%', // Ending top style attribute
+            }
+            );
+        });
+    $(document).ready(function(){
+        // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+        $('#modalps').modal({ 
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .6, // Opacity of modal background
+            inDuration: 500, // Transition in duration
+            outDuration: 500, // Transition out duration
+            startingTop: '5%', // Starting top style attribute
+            endingTop: '15%', // Ending top style attribute
+            }
+            );
+        });
+    $(document).ready(function(){
+        // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+        $('#modalra').modal({ 
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .6, // Opacity of modal background
+            inDuration: 500, // Transition in duration
+            outDuration: 500, // Transition out duration
+            startingTop: '5%', // Starting top style attribute
+            endingTop: '15%', // Ending top style attribute
+            }
+            );
+        });
+    $(document).ready(function(){
+        // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+        $('#modaltd').modal({ 
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .6, // Opacity of modal background
+            inDuration: 500, // Transition in duration
+            outDuration: 500, // Transition out duration
+            startingTop: '5%', // Starting top style attribute
+            endingTop: '15%', // Ending top style attribute
+            }
+            );
+        });
+    $(document).ready(function(){
+        // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+        $('#modalvm').modal({ 
+            dismissible: true, // Modal can be dismissed by clicking outside of the modal
+            opacity: .6, // Opacity of modal background
+            inDuration: 500, // Transition in duration
+            outDuration: 500, // Transition out duration
+            startingTop: '5%', // Starting top style attribute
+            endingTop: '15%', // Ending top style attribute
+            }
+            );
+        });
